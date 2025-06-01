2ccebed28294da88b8d6a3f004fca0e7d5ea4be2
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-06-01 13:54:56 -0300

    consult for url

--- src/index.mjs
+++ src/index.mjs
@@ -1,54 +1,56 @@
 import https from 'https';
 
 export const handler = async (event) => {
-    const cep = event.cep?.replace(/\D/g, '');
+    const queryString = event.queryStringParameters;
+    const cep = queryString?.cep?.replace(/\D/g, '');
 
     if (!cep || cep.length !== 8) {
         return {
             statusCode: 400,
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ error: 'CEP inválido. Informe um CEP com 8 dígitos.' }),
         };
     }
 
     const url = `https://viacep.com.br/ws/${cep}/json/`;
 
-    const dados = await new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            let data = '';
+    try {
+        const dados = await new Promise((resolve, reject) => {
+            https.get(url, (res) => {
+                let data = '';
 
-            res.on('data', chunk => {
-                data += chunk;
-            });
+                res.on('data', chunk => {
+                    data += chunk;
+                });
 
-            res.on('end', () => {
-                const json = JSON.parse(data);
-                if (json.erro) {
-                    reject('CEP não encontrado.');
-                } else {
-                    resolve(json);
-                }
+                res.on('end', () => {
+                    const json = JSON.parse(data);
+                    if (json.erro) {
+                        reject('CEP não encontrado.');
+                    } else {
+                        resolve(json);
+                    }
+                });
+            }).on('error', (e) => {
+                reject(e);
             });
-        }).on('error', (e) => {
-            reject(e);
         });
-    }).catch(err => {
-        return { error: err };
-    });
 
-    if (dados.error) {
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                logradouro: dados.logradouro,
+                bairro: dados.bairro,
+                cidade: dados.localidade,
+                estado: dados.uf,
+            }),
+        };
+    } catch (error) {
         return {
             statusCode: 404,
-            body: JSON.stringify({ error: dados.error }),
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: error.toString() }),
         };
     }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            logradouro: dados.logradouro,
-            bairro: dados.bairro,
-            cidade: dados.localidade,
-            estado: dados.uf,
-        }),
-    };
 };
